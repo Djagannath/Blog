45641f9e66c0a683fc31c37d386943b6a5006859
Oleynicov EV <eugenefilter@gmail.com>
2020-08-22 23:17:22 +0300

    frontend settings

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,14 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.js('resources/admin/js/app.js', 'public/admin/js')
+  .sass('resources/admin/sass/app.scss', 'public/admin/css');
+
+// for production add version for css and js
+if (mix.inProduction()) {
+  mix.version();
+}
+
+if (!mix.inProduction()) {
+  mix.browserSync('127.0.0.1');
+}
